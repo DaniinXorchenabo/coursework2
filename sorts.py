0acb0b346817d37bd604d7a2df1dad58a152ad93
DaniinXorchenabo <rkbcu@mail.ru>
2021-11-24 10:07:45 +0300

    added `HeapSort`.

--- sorts.py
+++ sorts.py
@@ -37,7 +37,55 @@ class HeapSort(BaseSort, ABC):
 
     @classmethod
     def sort(cls, a: ArrayType) -> float:
-        return 15.1
+        start_time = time()
+
+        def _shift(a: ArrayType[int], index, l, r):
+
+            def print_heap(*a, **k):
+                pass
+
+            if r > (ind2 := l + index * 2 + 1):
+                if a[(ind1 := l + index * 2)] > a[ind2]:
+                    if a[(ind := l + index)] < a[ind1]:
+                        a[ind], a[ind1] = a[ind1], a[ind]
+                        a = _shift(a, index * 2, l, r)
+                        print_heap(a)
+                        return a
+                else:
+                    if a[(ind := l + index)] < a[ind2]:
+                        a[ind], a[ind2] = a[ind2], a[ind]
+                        a = _shift(a, index * 2 + 1, l, r)
+                        print_heap(a)
+                        return a
+            elif r > (ind1 := l + index * 2):
+                if a[(ind := l + index)] < a[ind1]:
+                    a[ind], a[ind1] = a[ind1], a[ind]
+                    a = _shift(a, index * 2, l, r)
+                    print_heap(a)
+                    return a
+            print_heap(a)
+            return a
+
+        def create_heap(a: ArrayType[int]) -> ArrayType[int]:
+            a = [float("-inf")] + a
+            [_shift(a, i, 0, len(a)) for i in range(len(a) // 2 - 1, 0, -1)]
+            return a
+
+        def _sort_shift(a: ArrayType[int], min_el_index, next_index) -> None:
+            # print(a)
+            a[min_el_index], a[next_index] = a[next_index], a[min_el_index]
+            # print(a)
+            _shift(a, min_el_index, 0, next_index)
+            return None
+
+        def _heap_sort(a: ArrayType[int]) -> ArrayType[int]:
+            [(_sort_shift(a, 1, i), print(i)) for i in range(len(a) - 1, 0, -1)]
+            return a
+
+        a = create_heap(a)
+        a = _heap_sort(a)
+        # print(a)
+        return time() - start_time
 
 
 class QuickSort(BaseSort, ABC):
